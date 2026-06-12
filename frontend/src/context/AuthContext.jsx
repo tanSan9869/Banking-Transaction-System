033385a1b8a8a478bf103319a03aa59c12fa67d2
Tanishk Sancheti <tanishksancheti1@gmail.com>
2026-06-12 08:10:43 +0530

    AuthContext Config

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,60 @@
+import {  createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api/axios";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) =>{
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+    const token = localStorage.getItem('token');
+    const userData = localStorage.getItem('user');
+    if (token && userData) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setUser(JSON.parse(userData));
+    }
+    setLoading(false);
+  }, []);
+
+    
+    const login = async (email,password) =>{
+        const res = await api.post('/auth/login',{email,password});
+        localStorage.setItem('token',res.data.token);
+        localStorage.setItem('user', JSON.stringify({
+            email:res.data.email,
+            fullName:res.data.fullName
+        }));
+        setUser({email:res.data.email,fullName:res.data.fullName});
+        navigate('/dashboard');
+    };
+
+    const register = async (fullName,email,password) => {
+        const res = await api.post('/auth/register',{fullName,email,password});
+        localStorage.setItem('token',res.data.token);
+        localStorage.setItem('user',JSON.stringify({
+            email:res.data.email,
+            fullName:res.data.fullName
+        }));
+        setUser({email:res.data.email,fullName:res.data.fullName});
+        navigate('/dashboard');
+    };
+
+    const logout = ()=>{
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        setUser(null);
+        navigate('/login');
+    };
+
+    return(
+        <AuthContext.Provider value={{user,loading,login,register,logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+    };
+
+export default AuthContext;
